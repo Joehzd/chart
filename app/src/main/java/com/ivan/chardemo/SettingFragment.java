d0df1859e8719b376fba383e3a009645edef5a12
ivan <mx_visionary@126.com>
2017-05-16 22:45:02 +0800

    fix

--- app/src/main/java/com/ivan/chardemo/SettingFragment.java
+++ app/src/main/java/com/ivan/chardemo/SettingFragment.java
@@ -2,6 +2,7 @@ package com.ivan.chardemo;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
@@ -10,6 +11,7 @@ import android.support.v4.content.SharedPreferencesCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by a123 on 2017/5/16.
@@ -22,7 +24,28 @@ public class SettingFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.preference);
         SharedPreferences preferences= PreferenceManager.getDefaultSharedPreferences(getActivity());
         boolean temperature_unit= preferences.getBoolean("temperature_unit",true);
-
+        System.out.println(temperature_unit+"==unit");
+        Preference preference=findPreference("temperature_unit");
+        Preference tip=findPreference("temperature_tip");
+        Preference smartLock=findPreference("smartLock");
+        smartLock.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getActivity(),"smartlock",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        preference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                if ((boolean) newValue){
+                    System.out.println("我变true");
+                }else {
+                    System.out.println("我变false");
+                }
+                return true;
+            }
+        });
 
     }
 }
