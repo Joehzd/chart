50993f6123124ab12155f9759005cd2a246a0561
zhangyicheng4211 <zhangyicheng@jccy-tech.com>
2017-05-18 16:17:04 +0800

    test

--- app/src/main/java/com/ivan/chardemo/MainActivity.java
+++ app/src/main/java/com/ivan/chardemo/MainActivity.java
@@ -1,14 +1,25 @@
 package com.ivan.chardemo;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.widget.TextView;
+
+import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView textView;
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
             switch (msg.what){
@@ -25,6 +36,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView= (TextView) findViewById(R.id.text);
+        //Spannable spannable=
+
+        Drawable drawable = getResources().getDrawable(R.drawable.temperature_oval);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        //需要处理的文本，0是需要被替代的文本
+        SpannableString spannable = new SpannableString("999");
+        //要让图片替代指定的文字就要用ImageSpan
+        ImageSpan span = new ImageSpan(drawable, ImageSpan.ALIGN_BASELINE);
+        //开始替换，注意第2和第3个参数表示从哪里开始替换到哪里替换结束（start和end）
+        //最后一个参数类似数学中的集合,[5,12)表示从5到12，包括5但不包括12
+        spannable.setSpan(span, 1,"999".length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        textView.setText(spannable);
 //        chartView= (ChartView) findViewById(R.id.chart);
 //        new Thread(new Runnable() {
 //            @Override
@@ -40,10 +64,10 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        }).start();
 
-        Intent intent=new Intent(this,SettingActivity.class);
-
-
-        startActivity(intent);
+//        Intent intent=new Intent(this,SettingActivity.class);
+//
+//
+//        startActivity(intent);
 //        chartView= (ChartView) findViewById(R.id.chart);
 //        new Thread(new Runnable() {
 //            @Override
